b9a40ae0dc586bdcdfe7472c1bac556b02494da9
Idrec <narzievuktam@gmail.com>
2025-05-26 13:04:40 +0500

    new

---
+++ pro unity 1 dars/js/script.js
@@ -0,0 +1,96 @@
+// == kirish
+
+// console.log("war")
+// console.log(1+3+9)
+
+// // bir qatorli izoh
+// /*
+// ko'p qatorli izoh
+// */
+
+// /** JSDoc izoh
+//  * taks - 1
+//  * taks - 2
+//  * taks - 3
+//  * 
+//  */
+
+
+
+
+// === 1  dars
+
+/** o'zgaruvchilar e'lon qilish
+ * var = ES5
+ * let = ES6
+ * cons = ES6
+ */
+//  EcmaSript
+
+
+// // var
+// var value; //var orqali o'zgaruvchi yaratdik, value - bu o'zgaruvchilar nomi
+// value = 'hello'; // value nomli o'zgaruvchi murojat qilib tenglik belgisi "=" orqali unga qiymat o'rnatdik
+// value= 123; // value nomli o'zgaruvchi murojat qilib uning qiymatini qayta yozib o'zgartirish mumkin
+
+// var value = "hello"; // var orqali yaratilgan o'zgaruvchiga to'g'ridan to'g'ri qiymat yozish mumkin 
+// value = 'world';
+
+// var value = "hello";
+// var value = "world"; // var bir xil identikator(nomli) o'zgaruvchi yaratishga imkoniyat beradi va bu koddagi konfliktlarga olib kelashi mumkin!
+// var value = 'hello';
+
+
+
+// let 
+// let value = 'hello';
+// value = 'world';
+// value = 123;
+
+// let value = 1
+// let value = 2 // let bir xil identikator(nomli) o'zgaruvchi yaratishga imkoniyat bermaydi!
+
+
+// console.log(value);
+// let value = 'hello' // let o'zgaruvchi e'lon qilishdan oldin foydalanishga imkoniyat bermaydi!
+
+//o'zgaruvchilar nomlash
+
+// let value;
+// let Value;
+// let value1;
+// let _value;
+// let value_;
+// let _;
+// let $value;
+// let value$;
+// let $;
+
+// mumkin emas
+// let let;
+// let var;
+// let const;
+// let return;
+// let alert;
+// let 1value;
+// let #value;
+// let #value;
+
+
+// let leftBorderWidth; // camelCase
+// let left_border_width; // under_score, snake_case
+// let LEFT_BORDER_WIDTH; // uppercase
+// let LeftBorderWidth; // pascalCase
+
+
+// const
+// const value;
+// const value = "hello"
+// value = 'world' // const orqali e'lon qilingan o'zgaruvchilar qiymati o'zgartirib bo'lmaydi
+
+// const value = 'hello' // const bir xil identikator(nomli) o'zgaruvchi yaratishga imkoniyat bermaydi!
+
+// console.log(value) // const o'zgaruvchi e'lon qilishdan oldin foydalanishga imkoniyat bermaydi!
+//const value = 'hello'
+
+const API_KEY = '123ndan123123';
