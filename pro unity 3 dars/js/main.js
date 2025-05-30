4bfa5d7c39fe991b6040b155452d6fff91da7c7f
Idrec <narzievuktam@gmail.com>
2025-05-30 12:42:53 +0500

    new

---
+++ pro unity 3 dars/js/main.js
@@ -0,0 +1,129 @@
+'use strict';
+
+/**
+ * string
+ * number 
+ * boolean
+ * undefined
+ * null
+ * symbol
+ * object
+ * array
+ * function
+ * object date
+ */
+
+
+let value
+
+// number to string
+let num = 13
+value = num
+
+
+value = String(13);
+value = String(num); 
+value = (7).toString(); 
+value = num.toString();
+value = 8 + '';
+value = num + '';
+
+
+// boolean to string
+let bool = true;
+
+value = String(false);
+value = String(bool); 
+value = (false).toString(); 
+value = bool.toString();
+value = false + '';
+value = bool + '';
+
+
+// undefined to string
+let example = undefined;
+
+value = example;
+value = String(example); 
+// value = (example).toString(); // undefined ga metod qo'llabbulmaydi!
+value = example + '';
+
+
+// null to string
+example = null;
+
+value = example;
+value = String(null);
+value = String(example);
+// value = (null),toString(); // null ga metod qo'llab bo'lmaydi!
+value = null + '';
+value = example + '';
+
+
+// object to string
+let user = {
+    name:  'Harry potter',
+    age: 34
+}
+
+value = user;
+value = String(user); // [object Object]
+value = user.toString(); // [object Object]
+value = user + ''; //[object Object]
+
+
+// array to string
+let array = ['hello', false, 123];
+
+value = array;
+value = String(array); // hello,false,123
+value = array.toString(); // hello,false,123
+value = array + ''; // hello,false,123
+
+
+
+
+// JSON
+value = JSON;
+value = JSON.stringify(user); //'{"name":"Harry potter","age":34}'
+value = JSON.stringify(array); //["hello",false,123] 
+
+
+
+
+
+
+// === string to number
+
+let str = '13';
+
+value = str;
+value = Number('76');
+value = Number(str);
+value = +'78'; // unar operatori o'zidan keyn turgan qiymatni songa aylantiradi
+value = +str;
+
+// Agar son harf bilan kelsa
+
+value = +'12ab'; // NaN = not a number
+value = Number('231as'); // NaN
+
+
+value = +''; // 0
+value = Number(''); // 0
+
+value = '100USD';
+value = parseInt('100USD'); //matndan sonni butun qismini 100 ni olib beradi
+value = parseFloat('12.5USD'); // 12
+value = parseFloat('13.5USD'); // 13.5
+value = parseInt('USD102'); // NaN yani son faqat matndan oldinga kelsa parse Int va Float komandalari ishlaydi
+value = parseFloat('USD23.3'); // NaN yani son faqat matndan oldinga kelsa parse Int va Float komandalari ishlaydi
+
+
+// boolean to number
+
+
+
+
+console.log(value);
+console.log(typeof value)
