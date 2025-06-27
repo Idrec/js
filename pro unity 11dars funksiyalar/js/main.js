6c171835ac8eca5585ac3306a0815f0f2cca1b79
Idrec <narzievuktam@gmail.com>
2025-06-27 11:34:01 +0500

    new

---
+++ pro unity 11dars funksiyalar/js/main.js
@@ -0,0 +1,117 @@
+'use strict'
+
+// let username
+// username = 'john'
+// username = 'harry'
+
+// console.log('hello' + username);
+
+
+/**Funksiyalar
+ * declaration(an`anaviy)
+ * expression (kalta - o'zgaruvchiga yozilgan)
+ * arrow(strelkali funksiyalar)
+ */
+
+// declaration
+
+/*
+    function nameOfFunction(parametrs){
+        action of function
+    }
+
+*/
+
+// function myFunc(){
+//     console.log('HELLO MY FRIEND')
+// }
+
+// myFunc()
+
+
+/// Parametr/argument bilan ammallar
+// function sayHello(name){
+//     console.log('hello' + name);
+// }
+
+// sayHello('Thomas Shelby')
+
+
+// function sayHello(firtsname, lastname){
+//     let str;
+//     str = 'hello' + firtsname + ' ' + lastname;
+//     console.log(str)
+// }
+
+// sayHello('john', 'bastard')
+
+
+//argument bilan amallar
+// function myFunc(){
+//     console.log(arguments)
+// }
+// myFunc(123, 'hello', false)
+
+
+// function getSumm(){
+//     let res = 0;
+//     for(let value of arguments){
+//         res += value
+//     }
+//     console.log(res);
+// }
+
+// getSumm(10,13,17,26)
+
+
+//Kor'rinish tartibi
+//== 1
+// let x = 10;
+
+// function myFunc(){
+//     x = 13;
+// }
+// myFunc()
+
+//== 2
+// function myFunc(){
+//    let x = 13;
+// }
+// myFunc()
+
+// x = 13; // x is not defined
+// console.log(x);
+
+
+//== 3
+// function myFunc(){
+//     value = 13;
+// }
+// myFunc(value) // value is not defined
+
+//return
+
+// function sayHello(name){
+//     let str;
+//     str = 'hello' + ' ' + name;
+//     return str;
+// }
+
+// let result = sayHello('harry');
+// console.log(result)
+
+
+// function getSumm(){
+//     let res = 0;
+//     for(let value of arguments){
+//         res +=value
+//     }
+//     return res;
+// }
+// let result = getSumm(10,25,12,17)
+// console.log(result)
+
+
+function aboutNumbers(num){
+    if
+}
